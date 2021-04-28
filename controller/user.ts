8ba25a1e25736f9eaec084a3c98706b391d7512f
Jung SongYi <lovelysi0113@gmail.com>
2021-04-28 21:49:41 +0900

    Add #5 controller about login OAuth with Google

--- controller/user.ts
+++ controller/user.ts
@@ -10,7 +10,40 @@ const userController = {
 		// 로그인 - OAuth 방식
 		let oauthServer = req.body.oauthServer;
 		if (oauthServer === 'google') {
-			//
+			const googleLoginURL = 'https://accounts.google.com/o/oauth2/token';
+			const googleInfoURL = 'https://www.googleapis.com/oauth2/v3/userinfo';
+			// authorization code를 이용해 access token을 발급받음
+			const accessToken = axios
+				.post(googleLoginURL, {
+					client_id: process.env.GOOGLE_CLIENT_ID,
+					client_secret: process.env.GOOGLE_CLIENT_SECRET,
+					code: req.body.authorizationCode,
+					redirect_uri: 'http://localhost:3000', // 클라이언트 리디렉션 uri, 나중에 수정해야함
+					grant_type: 'authorization_code',
+				})
+				.then(result => result.data.access_token)
+				.catch(err => {
+					console.log(err.message);
+					res.status(400).json({
+						message: 'error',
+					});
+				});
+			// accessToken을 통해 로그인한 유저 정보 가져오기
+			const resInfo = await axios
+				.post(googleInfoURL, {
+					headers: {
+						authorization: `Bearer ${accessToken}`,
+					},
+				})
+				.then(result => result.data.email)
+				.catch(err => {
+					console.log(err.message);
+				});
+			// 여기서 바로 데이터베이스랑 통신해서 유저정보를 저장하는 것도 좋을듯 -> 우선 보류
+			// response 보내기
+			res.status(200).json({
+				accessToken,
+			});
 		} else if (oauthServer === 'github') {
 			const githubLoginURL = 'https://github.com/login/oauth/access_token';
 			const githubInfoURL = 'https://api.github.com/user';
