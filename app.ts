101798f232e9467a3dfac0256f4c0e33d05b1112
katsukichi <nsg8957@naver.com>
2021-04-28 22:35:44 +0900

    Add #10 something dependancy added & emailauth controller added but not yet done

--- app.ts
+++ app.ts
@@ -1,35 +1,37 @@
 import express from 'express';
 import cors from 'cors';
-import './init/mongo'
-import "dotenv/config";
-import devRouter from './routes/dev'
+import './init/mongo';
+import 'dotenv/config';
+import devRouter from './routes/dev';
+const cookieParser = require('cookie-parser');
 class App {
-  public application: express.Application;
+	public application: express.Application;
 
-  constructor() {
-    this.application = express();
-    this.router();
-  }
+	constructor() {
+		this.application = express();
+		this.router();
+	}
 
-  private router(): void {
-    this.application.get('/', (req: express.Request, res: express.Response) => {
-      res.send('hello! world!');
-    })
-  }
+	private router(): void {
+		this.application.get('/', (req: express.Request, res: express.Response) => {
+			res.send('hello! world!');
+		});
+	}
 }
 
 const app = new App().application;
 
 const corsOption = {
-  origin: true,
-  method: ['post','get','delete','options'],
-  credentials: true
-}
-
-app.use(express.json())
+	origin: true,
+	method: ['post', 'get', 'delete', 'options'],
+	credentials: true,
+};
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.json());
 app.use(cors(corsOption));
-app.use('/',devRouter)
+app.use('/', devRouter);
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+	console.log('Server listening on port 3000');
 });
 export default app;
