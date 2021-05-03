27a78867a42c3562dc7229840d2c2edfabbbef2e
katsukichi <nsg8957@naver.com>
2021-05-03 15:32:00 +0900

    Add #58 delete comment feature implement

--- controller/board_comment.ts
+++ controller/board_comment.ts
@@ -32,18 +32,37 @@ const commentController = {
 			.save()
 			.then(async result => {
 				console.log(result);
-				const foundData = await commentModel.find({ board_id });
-				return res.status(201).json({ commentAll: foundData });
+				const commentAll = await commentModel.find({ board_id });
+				return res.status(201).json({
+					commentAll,
+				});
 			})
 			.catch(error => {
 				return res.status(500).json({
 					message: error.message,
-					error,
 				});
 			});
 	},
-	commentDelete: (req: Request, res: Response) => {
+	commentDelete: async (req: Request, res: Response) => {
 		// 댓글 삭제하기
+		console.log('💜commentDelete ', req.params);
+		const board_id = Number(req.params.board_id);
+		const comment_id = Number(req.params.comment_id);
+		const user_id = req.user_id;
+		const user_email = req.user_email;
+		commentModel
+			.deleteOne({ $and: [{ user_id }, { id: comment_id }] })
+			.then(async data => {
+				const commentAll = await commentModel.find({ board_id });
+				res.status(200).json({
+					commentAll,
+				});
+			})
+			.catch(err => {
+				res.status(500).json({
+					message: err.message,
+				});
+			});
 	},
 };
 
--- src/db/models/comment.ts
+++ src/db/models/comment.ts
@@ -19,6 +19,10 @@ const commentSchema = new mongoose.Schema(
 		board_id: { type: Number, require: true },
 		// 그 게시글의 아이디는 고유해야하고, 게시글당 하나의 레코즈를 가진다.
 		comment_body: { type: String, default: null, require: true },
+		// 대댓글은 어떻게 관리하는게 좋을까..
+
+		// parent_id, children을 가지고 관리하면 될거같은데..!
+		// 트리 구조 느낌으로 가면 될듯..! 흠.. 좀더 고민해보기!!
 	},
 	{
 		timestamps: true,
