ab98bffd0640f97f46ffa3f043b90abcb2358625
katsukichi <nsg8957@naver.com>
2021-05-03 11:44:31 +0900

    Add #57 #59 board Comment Add feature implement

--- controller/board_comment.ts
+++ controller/board_comment.ts
@@ -1,9 +1,46 @@
 import { Request, Response } from 'express';
 import { Boards } from '../src/db/models/board';
+import mongoose from 'mongoose';
+import commentModel from '../src/db/models/comment';
 
+// interface
+interface Comment extends mongoose.Document {
+	user_id: string;
+	user_email: string;
+	comment_body: string;
+	// children: Array<Comment>;
+	board_id: number;
+}
 const commentController = {
 	commentAdd: (req: Request, res: Response) => {
 		// 댓글 추가하기
+		console.log(req.body);
+		console.log('💜commentAdd ', req.params);
+		const user_id = req.user_id;
+		const user_email = req.user_email;
+		const board_id = Number(req.params.board_id);
+		const { comment_body } = req.body;
+
+		const comment = new commentModel({
+			board_id,
+			user_id,
+			user_email,
+			comment_body,
+		});
+		// new mongoose.Types.ObjectId 유니크값 생성기 같은 느낌이다. 아이디생성용
+		return comment
+			.save()
+			.then(async result => {
+				console.log(result);
+				const foundData = await commentModel.find({ board_id });
+				return res.status(201).json({ commentAll: foundData });
+			})
+			.catch(error => {
+				return res.status(500).json({
+					message: error.message,
+					error,
+				});
+			});
 	},
 	commentDelete: (req: Request, res: Response) => {
 		// 댓글 삭제하기
--- src/db/models/comment.ts
+++ src/db/models/comment.ts
@@ -2,10 +2,11 @@ import mongoose from 'mongoose';
 
 // interface
 interface Comment extends mongoose.Document {
-	userId: number;
-	content: string;
-	children: Array<Comment>;
-	createdAt: Date;
+	user_email: string;
+	comment_body: string;
+	user_id: number;
+	// children: Array<Comment>;
+	board_id: number;
 }
 
 // Define Schemes
@@ -13,16 +14,18 @@ const commentSchema = new mongoose.Schema(
 	{
 		//   id: {type:Number,required:true,unique:true},
 		//id 테이블은 비공개로 생성된 _id로
-		boardId: { type: String, require: true, unique: true },
+		user_email: { type: String, require: true },
+		user_id: { type: Number, require: true },
+		board_id: { type: Number, require: true },
 		// 그 게시글의 아이디는 고유해야하고, 게시글당 하나의 레코즈를 가진다.
-		comment: { type: Array<Comment>(), default: null },
+		comment_body: { type: String, default: null, require: true },
 	},
 	{
 		timestamps: true,
 	},
 );
-commentSchema.static('findOneOrCreate', async function findOneOrCreate(boardId, doc: Comment) {
-	const one = await this.findOne({ boardId });
+commentSchema.static('findOneOrCreate', async function findOneOrCreate(board_id, doc: Comment) {
+	const one = await this.findOne({ board_id });
 	console.log(one);
 	console.log(doc);
 });
