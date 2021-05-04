13ba597a87319cfecc32843b7b7bf3255f4ea6b2
katsukichi <nsg8957@naver.com>
2021-05-04 16:26:17 +0900

    Fix #67 #62 AuthChecker edited

--- middleware/authChecker.ts
+++ middleware/authChecker.ts
@@ -20,28 +20,28 @@ export const authChecker = async (req: Request, res: Response, next: NextFunctio
 					if (err) {
 						// access toekn : 기간 만료
 						const refreshToken = req.cookies.refreshToken;
-						if (refreshToken) {
-							// refresh token : 존재 -> 정상인지 확인해야함
-							jwt.verify(
-								refreshToken,
-								process.env.REFRESH_SECRET as string,
-								async (err: VerifyErrors | null, decoded: any | undefined) => {
-									if (err) {
-										// refresh token : 정상적이지 않음 -> 로그인으로 돌아감
-										res.redirect(`${process.env.CLIENT_URL}/login`);
-									} else {
-										// 새로운 access token을 발급 받음
-										const id = decoded.userId;
-										const email = decoded.email;
-										const newAccessToken = await accessTokenGenerator(id, email);
-										req.newAccessToken = newAccessToken;
-									}
-								},
-							);
-						} else {
-							// refresh token : 없음
-							res.redirect(`${process.env.CLIENT_URL}/login`);
-						}
+						// if (refreshToken) {
+						// 	// refresh token : 존재 -> 정상인지 확인해야함
+						// 	jwt.verify(
+						// 		refreshToken,
+						// 		process.env.REFRESH_SECRET as string,
+						// 		async (err: VerifyErrors | null, decoded: any | undefined) => {
+						// 			if (err) {
+						// 				// refresh token : 정상적이지 않음 -> 로그인으로 돌아감
+						// 				res.redirect(`${process.env.CLIENT_URL}/login`);
+						// 			} else {
+						// 				// 새로운 access token을 발급 받음
+						// 				const id = decoded.userId;
+						// 				const email = decoded.email;
+						// 				const newAccessToken = await accessTokenGenerator(id, email);
+						// 				req.newAccessToken = newAccessToken;
+						// 			}
+						// 		},
+						// 	);
+						// } else {
+						// 	// refresh token : 없음
+						// 	res.redirect(`${process.env.CLIENT_URL}/login`);
+						// }
 					} else {
 						// access token : 만료되지 않음
 						req.user_email = decoded.email;
@@ -52,23 +52,23 @@ export const authChecker = async (req: Request, res: Response, next: NextFunctio
 		} else if (LoginType === 'google') {
 			// 로그인 방식 - google
 			// refresh token을 이용하여 새로운 access token을 발급받음
-			const googleLoginURL = 'https://accounts.google.com/o/oauth2/token';
-			await axios
-				.post(googleLoginURL, {
-					client_id: process.env.GOOGLE_CLIENT_ID,
-					client_secret: process.env.GOOGLE_CLIENT_SECRET,
-					grant_type: 'refresh_token',
-					refresh_token: req.cookies.refreshToken,
-				})
-				.then(async result => {
-					let accessToken = result.data.access_token;
-					req.newAccessToken = accessToken;
-				})
-				.catch(err => {
-					// 에러 발생 -> 인증 불가 -> 다시 로그인해야함
-					console.log('🔒error:google', err.message);
-					res.redirect(`${process.env.CLIENT_URL}/login`);
-				});
+			// const googleLoginURL = 'https://accounts.google.com/o/oauth2/token';
+			// await axios
+			// 	.post(googleLoginURL, {
+			// 		client_id: process.env.GOOGLE_CLIENT_ID,
+			// 		client_secret: process.env.GOOGLE_CLIENT_SECRET,
+			// 		grant_type: 'refresh_token',
+			// 		refresh_token: req.cookies.refreshToken,
+			// 	})
+			// 	.then(async result => {
+			// 		let accessToken = result.data.access_token;
+			// 		req.newAccessToken = accessToken;
+			// 	})
+			// 	.catch(err => {
+			// 		// 에러 발생 -> 인증 불가 -> 다시 로그인해야함
+			// 		console.log('🔒error:google', err.message);
+			// 		res.redirect(`${process.env.CLIENT_URL}/login`);
+			// 	});
 			// access token으로 유저 정보 가져오기
 			const googleInfoURL = 'https://www.googleapis.com/oauth2/v3/userinfo';
 			const resInfo = await axios
@@ -98,7 +98,7 @@ export const authChecker = async (req: Request, res: Response, next: NextFunctio
 		} else if (LoginType === 'github') {
 			// 로그인 방식 - github
 			// refresh token이 없음, 로그아웃 하기 전까지 access token 계속 사용 가능
-			req.newAccessToken = accessToken;
+			// req.newAccessToken = accessToken;
 			// access token으로 유저 정보 가져오기
 			const githubInfoURL = 'https://api.github.com/user';
 			const resInfo = await axios
@@ -137,9 +137,9 @@ export const authChecker = async (req: Request, res: Response, next: NextFunctio
 		);
 		if (req.newAccessToken !== undefined) {
 			// 새로운 access token 생성됨 -> 응답으로 보내주어야 함
-			res.status(200).json({
-				newAccessToken: req.newAccessToken,
-			});
+			// res.status(200).json({
+			// 	newAccessToken: req.newAccessToken,
+			// });
 		} else if (req.user_id !== undefined && req.user_email !== undefined) {
 			// 실제 요청으로 넘어감
 			console.log('🔒go next function!!');
@@ -148,7 +148,6 @@ export const authChecker = async (req: Request, res: Response, next: NextFunctio
 			// 에러 발생 -> 로그인 페이지로 돌아감
 			res.redirect(`${process.env.CLIENT_URL}/login`);
 		}
-		next();
 	} else {
 		// access token이 없을 때 -> 로그인 페이지로 돌아감
 		res.redirect(`${process.env.CLIENT_URL}/login`);
